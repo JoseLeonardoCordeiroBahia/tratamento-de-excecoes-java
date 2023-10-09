f10e96722995bc5f3135ce3d4618887b7d0c360c
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-09 00:06:53 -0300

    Bloco finally

---
+++ src/_4_bloco_finally/Program.java
@@ -0,0 +1,29 @@
+package _4_bloco_finally;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		File file = new File("C:\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
+	}
+	
+}
